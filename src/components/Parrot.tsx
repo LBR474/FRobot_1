f7010b875abf43e8296d98c0ddbb2d83cf05db37
Fake Name <fakename@fake.com>
2023-06-19 08:13:26 +1000

    Ad extra .GLBs

---
+++ src/components/Parrot.tsx
@@ -0,0 +1,34 @@
+
+
+import { useLoader, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Vector3 } from "three";
+
+
+
+
+interface ParrotProps {
+  scale: Vector3;
+}
+
+function Flamingo({ scale }: ParrotProps) {
+
+  
+  let mixer: THREE.AnimationMixer | null = null;
+  const { scene, animations } = useLoader(
+    GLTFLoader,
+   'Parrot.glb'
+  );
+  // console.log(scene);
+  mixer = new THREE.AnimationMixer(scene);
+  void mixer.clipAction(animations[0]).play();
+
+  useFrame((_state, delta) => {
+    mixer!.update(delta);
+    // console.log(ca);
+  });
+  return <primitive object={scene} position={[0, 0, 0]} scale={scale} />;
+}
+
+export default Parrot;
