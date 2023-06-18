a9b691ca421dd82569993e3164c08df433db4e5b
Fake Name <fakename@fake.com>
2023-06-18 18:39:26 +1000

    commit

--- src/Flamingo.tsx
+++ src/Flamingo.tsx
@@ -18,7 +18,7 @@ function Flamingo({ scale }: FlamingoProps) {
   let mixer: THREE.AnimationMixer | null = null;
   const { scene, animations } = useLoader(
     GLTFLoader,
-   '/Flamingo.glb'
+   'Flamingo.glb'
   );
   // console.log(scene);
   mixer = new THREE.AnimationMixer(scene);
