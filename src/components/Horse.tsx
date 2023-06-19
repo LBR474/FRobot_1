f391ccf36fa66901af62c651919218eadd05f470
Fake Name <fakename@fake.com>
2023-06-19 19:32:53 +1000

    extra commit

--- src/components/Horse.tsx
+++ src/components/Horse.tsx
@@ -4,6 +4,7 @@ import { useLoader, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Vector3 } from "three";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 
 
 
@@ -19,19 +20,22 @@ function Horse({ scale }: HorseProps) {
 
   
   let mixer: THREE.AnimationMixer | null = null;
-  const { scene, animations } = useLoader(
-    GLTFLoader,
-   'Horse.glb'
-  );
+
+  
+  const gltf = useLoader(GLTFLoader, "PrimaryIonDrive.glb", (loader) => {
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath("/node_modules/three/examples/jsm/libs/draco/");
+    loader.setDRACOLoader(dracoLoader);
+  });
   // console.log(scene);
-  mixer = new THREE.AnimationMixer(scene);
-  void mixer.clipAction(animations[0]).play();
+  mixer = new THREE.AnimationMixer(gltf.scene);
+  void mixer.clipAction(gltf.animations[0]).play();
 
   useFrame((_state, delta) => {
     mixer!.update(delta);
     // console.log(ca);
   });
-  return <primitive object={scene} position={[0, 0, 0]} scale={scale} />;
+  return <primitive object={gltf.scene} position={[0, 0, 0]} scale={scale} />;
 }
 
 export default Horse;
